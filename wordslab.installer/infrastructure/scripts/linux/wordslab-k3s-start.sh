4bd424a0ee839e5da305eacd6f9a61f23e8228ad
laurent prud'hon <laurent.prudhon@hotmail.fr>
2022-02-19 21:11:34 +0100

    Linux QEMU install OK

---
+++ wordslab.installer/infrastructure/scripts/linux/wordslab-k3s-start.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/bash
+mount -o bind /mnt/wordslab-cluster/etc/rancher /etc/rancher
+mount -o bind /mnt/wordslab-cluster/var/lib /var/lib
+mount -o bind /mnt/wordslab-cluster/var/log/rancher /var/log/rancher
+mount -o bind /mnt/wordslab-data/var/volume/rancher /var/volume/rancher
+
+rm -f /etc/rancher/k3s/k3s.yaml
+k3s --version | grep -o "v[0-9].*\s" > /var/lib/rancher/k3s/version
+
+nohup /usr/local/bin/k3s server --write-kubeconfig-mode 644 --https-listen-port 6443 --log /var/log/rancher/k3s/k3s-$(date +%Y%m%d-%H%M%S).log --default-local-storage-path /var/volume/rancher/k3s </dev/null >/dev/null 2>&1 &
+sleep 1
+
+ps | grep -o "[0-9].*k3s\sserver" | grep -Eo "^[0-9]+" > /var/lib/rancher/k3s/pid
---
+++ wordslab.installer/infrastructure/scripts/linux/wordslab-vm-create.sh
@@ -0,0 +1,5 @@
+﻿#!/bin/bash
+cloud-localds seed.img user-data.yaml metadata.yaml
+qemu-img create -b ubuntu-20.04-minimal-cloudimg-amd64.img -f qcow2 wordslab-os.img
+qemu-img create -f qcow2 wordslab-cluster.img 10G
+qemu-img create -f qcow2 wordslab-data.img 10G
