fdcf90f2eeb0996864b6694d4fe36d4ed4d9116b
laurent prud'hon <laurent.prudhon@hotmail.fr>
2022-03-06 23:36:13 +0100

    First test of release pipeline

--- wordslab.manager/ManagerWebApp.cs
+++ wordslab.manager/ManagerWebApp.cs
@@ -1,3 +1,6 @@
+using Microsoft.AspNetCore.Components.Web;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using wordslab.manager.os;
 
 namespace wordslab.manager;
@@ -6,6 +9,9 @@ public static class ManagerWebApp
 {
     public const int DEFAULT_PORT = 3088;
 
+    // This attribute is a workaround to fix a bug in Blazor server when publishing with assembly trimming
+    // https://github.com/dotnet/aspnetcore/issues/37143#issuecomment-931726256
+    [DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(HeadOutlet))]
     public static void Run(int? port, string[] args)
     {
         Console.WriteLine($"wordslab manager v{ManagerConsoleApp.Version}");
@@ -40,7 +46,11 @@ public static class ManagerWebApp
         // Open browser
         if (!builder.Environment.IsDevelopment())
         {
-            WebBrowser.Open(url);
+            try
+            {
+                WebBrowser.Open(url);
+            }
+            catch (Exception) { } // Ignore exceptions - best effort only
         }
         
         Console.WriteLine("Press Ctrl-C to exit the application ...");
