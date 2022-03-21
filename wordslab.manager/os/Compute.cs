a479d9dc58ceb5314d05b4bcdd279262e081a4ee
laurent prud'hon <laurent.prudhon@hotmail.fr>
2022-03-22 00:49:16 +0100

    Get CPU flags on Windows

--- wordslab.manager/os/Compute.cs
+++ wordslab.manager/os/Compute.cs
@@ -1,5 +1,7 @@
 ﻿using System.Management;
 using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace wordslab.manager.os
@@ -53,7 +55,7 @@ namespace wordslab.manager.os
             /// The second is an offset of 0Ch-0Fh, which is the EDX value that the instruction returns. 
             /// Only the first two bytes of the property are significant and contain the contents of the DX register at CPU reset—all others are set to 0 (zero), and the contents are in DWORD format.
             /// </summary>
-            public string ProcessorId { get; set; } = string.Empty;
+            public string FeatureFlags { get; set; } = string.Empty;
         }
 
         // Utility methods for Windows Management Infrastructure
@@ -68,7 +70,63 @@ namespace wordslab.manager.os
         private static string GetPropertyString(object obj)
         {
             return (obj is string str) ? str : string.Empty;
-        }        
+        }
+
+        // https://en.wikipedia.org/wiki/CPUID
+        private static string GetFeatureFlagsFromCpuId()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (RuntimeInformation.OSArchitecture == Architecture.X64)
+            {
+                (int eax1, int ebx1, int ecx1, int edx1) = X86Base.CpuId(0x00000001, 0x00000000);
+                (int eax7, int ebx7, int ecx7, int edx7) = X86Base.CpuId(0x00000007, 0x00000000);
+                (int eax8, int ebx8, int ecx8, int edx8) = X86Base.CpuId(unchecked((int)0x80000001), 0x00000000);
+                
+                for (byte pos = 0; pos < 32; pos++)
+                {
+                    if ((edx1 & (1 << pos)) != 0)
+                    {
+                        sb.Append(CpuIdFlags.edx1Feature[pos]);
+                        sb.Append(' ');
+                    }
+                }
+                for (byte pos = 11; pos < 32; pos++)
+                {
+                    if ((edx8 & (1 << pos)) != 0)
+                    {
+                        sb.Append(CpuIdFlags.edx8Feature[pos]);
+                        sb.Append(' ');
+                    }
+                }
+                for (byte pos = 0; pos < 32; pos++)
+                {
+                    if ((ecx1 & (1 << pos)) != 0)
+                    {
+                        sb.Append(CpuIdFlags.ecx1Feature[pos]);
+                        sb.Append(' ');
+                    }
+                }
+                for (byte pos = 0; pos < 32; pos++)
+                {
+                    if ((ecx8 & (1 << pos)) != 0)
+                    {
+                        sb.Append(CpuIdFlags.ecx8Feature[pos]);
+                        sb.Append(' ');
+                    }
+                }
+                for (byte pos = 0; pos < 32; pos++)
+                {
+                    if ((ebx7 & (1 << pos)) != 0)
+                    {
+                        sb.Append(CpuIdFlags.ebx7Feature[pos]);
+                        sb.Append(' ');
+                    }
+                }
+            }
+
+            return sb.ToString();
+        }
 
         public static CPUInfo GetCPUInfo()
         {
@@ -76,7 +134,7 @@ namespace wordslab.manager.os
             {
                 // https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/win32-processor
 
-                string query = "SELECT Manufacturer, Name, NumberOfCores, NumberOfLogicalProcessors, MaxClockSpeed, L2CacheSize, L3CacheSize, ProcessorId FROM Win32_Processor";
+                string query = "SELECT Manufacturer, Name, NumberOfCores, NumberOfLogicalProcessors, MaxClockSpeed, L2CacheSize, L3CacheSize FROM Win32_Processor";
                 using ManagementObjectSearcher mos = new ManagementObjectSearcher(_managementScope, query, _enumerationOptions);
                 using var mociter = mos.Get().GetEnumerator();
                 mociter.MoveNext();
@@ -89,8 +147,8 @@ namespace wordslab.manager.os
                     NumberOfLogicalProcessors = GetPropertyValue<uint>(mo["NumberOfLogicalProcessors"]),
                     MaxClockSpeedMhz = GetPropertyValue<uint>(mo["MaxClockSpeed"]),
                     L2CacheSizeKB = GetPropertyValue<uint>(mo["L2CacheSize"]),
-                    L3CacheSizeKB = GetPropertyValue<uint>(mo["L3CacheSize"]),
-                    ProcessorId = GetPropertyString(mo["ProcessorId"])
+                    L3CacheSizeKB = GetPropertyValue<uint>(mo["L3CacheSize"]),                                        
+                    FeatureFlags = GetFeatureFlagsFromCpuId()
                 };
                 return cpu;
             }
@@ -365,6 +423,190 @@ namespace wordslab.manager.os
             { /* no Nvidia GPU available */ }
 
             return gpus.Cast<GPUInfo>().ToList();
+        }
+    }
+
+    static class CpuIdFlags
+    {
+        public static string[] edx1Feature;
+        public static string[] ecx1Feature;
+
+        public static string[] edx8Feature;
+        public static string[] ecx8Feature;
+
+        public static string[] ebx7Feature;
+
+        static CpuIdFlags()
+        {
+            edx1Feature = new string[32];
+            edx1Feature[0] = "fpu";
+            edx1Feature[1] = "vme";
+            edx1Feature[2] = "de";
+            edx1Feature[3] = "pse";
+            edx1Feature[4] = "tsc";
+            edx1Feature[5] = "msr";
+            edx1Feature[6] = "pae";
+            edx1Feature[7] = "mce";
+            edx1Feature[8] = "cx8";
+            edx1Feature[9] = "apic";
+            edx1Feature[10] = "(reserved)";
+            edx1Feature[11] = "sep";
+            edx1Feature[12] = "mtrr";
+            edx1Feature[13] = "pge";
+            edx1Feature[14] = "mca";
+            edx1Feature[15] = "cmov";
+            edx1Feature[16] = "pat";
+            edx1Feature[17] = "pse36";
+            edx1Feature[18] = "psn";
+            edx1Feature[19] = "clflush";
+            edx1Feature[20] = "(reserved)";
+            edx1Feature[21] = "dts";
+            edx1Feature[22] = "acpi";
+            edx1Feature[23] = "mmx";
+            edx1Feature[24] = "fxsr";
+            edx1Feature[25] = "sse";
+            edx1Feature[26] = "sse2";
+            edx1Feature[27] = "ss";
+            edx1Feature[28] = "ht";
+            edx1Feature[29] = "tm";
+            edx1Feature[30] = "ia64";
+            edx1Feature[31] = "pbe";
+
+            ecx1Feature = new string[32];
+            ecx1Feature[0] = "sse3";
+            ecx1Feature[1] = "pclmulqdq";
+            ecx1Feature[2] = "dtes64";
+            ecx1Feature[3] = "monitor";
+            ecx1Feature[4] = "ds-cpl";
+            ecx1Feature[5] = "vmx";
+            ecx1Feature[6] = "smx";
+            ecx1Feature[7] = "est";
+            ecx1Feature[8] = "tm2";
+            ecx1Feature[9] = "ssse3";
+            ecx1Feature[10] = "cnxt-id";
+            ecx1Feature[11] = "sdbg";
+            ecx1Feature[12] = "fma";
+            ecx1Feature[13] = "cx16";
+            ecx1Feature[14] = "xtpr";
+            ecx1Feature[15] = "pdcm";
+            ecx1Feature[16] = "(reserved)";
+            ecx1Feature[17] = "pcid";
+            ecx1Feature[18] = "dca";
+            ecx1Feature[19] = "sse4_1";
+            ecx1Feature[20] = "sse4_2";
+            ecx1Feature[21] = "x2apic";
+            ecx1Feature[22] = "movbe";
+            ecx1Feature[23] = "popcnt";
+            ecx1Feature[24] = "tsc-deadline";
+            ecx1Feature[25] = "aes";
+            ecx1Feature[26] = "xsave";
+            ecx1Feature[27] = "osxsave";
+            ecx1Feature[28] = "avx";
+            ecx1Feature[29] = "f16c";
+            ecx1Feature[30] = "rdrand";
+            ecx1Feature[31] = "hypervisor";
+
+            edx8Feature = new string[32];
+            edx8Feature[0] = "fpu";
+            edx8Feature[1] = "vme";
+            edx8Feature[2] = "de";
+            edx8Feature[3] = "pse";
+            edx8Feature[4] = "tsc";
+            edx8Feature[5] = "msr";
+            edx8Feature[6] = "pae";
+            edx8Feature[7] = "mce";
+            edx8Feature[8] = "cx8";
+            edx8Feature[9] = "apic";
+            edx8Feature[10] = "(reserved)";
+            edx8Feature[11] = "syscall";
+            edx8Feature[12] = "mtrr";
+            edx8Feature[13] = "pge";
+            edx8Feature[14] = "mca";
+            edx8Feature[15] = "cmov";
+            edx8Feature[16] = "pat";
+            edx8Feature[17] = "pse36";
+            edx8Feature[18] = "(reserved)";
+            edx8Feature[19] = "mp";
+            edx8Feature[20] = "nx";
+            edx8Feature[21] = "(reserved)";
+            edx8Feature[22] = "mmxext";
+            edx8Feature[23] = "mmx";
+            edx8Feature[24] = "fxsr";
+            edx8Feature[25] = "fxsr_opt";
+            edx8Feature[26] = "pdpe1gb";
+            edx8Feature[27] = "rdtscp";
+            edx8Feature[28] = "(reserved)";
+            edx8Feature[29] = "lm";
+            edx8Feature[30] = "3dnowext";
+            edx8Feature[31] = "3dnow";
+
+            ecx8Feature = new string[32];
+            ecx8Feature[0] = "lahf_lm";
+            ecx8Feature[1] = "cmp_legacy";
+            ecx8Feature[2] = "svm";
+            ecx8Feature[3] = "extapic";
+            ecx8Feature[4] = "cr8_legacy";
+            ecx8Feature[5] = "abm";
+            ecx8Feature[6] = "sse4a";
+            ecx8Feature[7] = "misalignsse";
+            ecx8Feature[8] = "3dnowprefetch";
+            ecx8Feature[9] = "osvw";
+            ecx8Feature[10] = "ibs";
+            ecx8Feature[11] = "xop";
+            ecx8Feature[12] = "skinit";
+            ecx8Feature[13] = "wdt";
+            ecx8Feature[14] = "(reserved)";
+            ecx8Feature[15] = "lwp";
+            ecx8Feature[16] = "fma4";
+            ecx8Feature[17] = "tce";
+            ecx8Feature[18] = "(reserved)";
+            ecx8Feature[19] = "nodeid_msr";
+            ecx8Feature[20] = "(reserved)";
+            ecx8Feature[21] = "tbm";
+            ecx8Feature[22] = "topoext";
+            ecx8Feature[23] = "perfctr_core";
+            ecx8Feature[24] = "perfctr_nb";
+            ecx8Feature[25] = "(reserved)";
+            ecx8Feature[26] = "dbx";
+            ecx8Feature[27] = "perftsc";
+            ecx8Feature[28] = "pcx_l2i";
+            ecx8Feature[29] = "(reserved)";
+            ecx8Feature[30] = "(reserved)";
+            ecx8Feature[31] = "(reserved)";
+
+            ebx7Feature = new string[32];
+            ebx7Feature[0] = "fsgsbase";
+            ebx7Feature[1] = "";
+            ebx7Feature[2] = "sgx";
+            ebx7Feature[3] = "bmi1";
+            ebx7Feature[4] = "hle";
+            ebx7Feature[5] = "avx2";
+            ebx7Feature[6] = "";
+            ebx7Feature[7] = "smep";
+            ebx7Feature[8] = "bmi2";
+            ebx7Feature[9] = "erms";
+            ebx7Feature[10] = "invpcid";
+            ebx7Feature[11] = "rtm";
+            ebx7Feature[12] = "pqm";
+            ebx7Feature[13] = "";
+            ebx7Feature[14] = "mpx";
+            ebx7Feature[15] = "pqe";
+            ebx7Feature[16] = "avx512_f";
+            ebx7Feature[17] = "avx512_dq";
+            ebx7Feature[18] = "rdseed";
+            ebx7Feature[19] = "adx";
+            ebx7Feature[20] = "smap";
+            ebx7Feature[21] = "avx512_ifma";
+            ebx7Feature[22] = "pcommit";
+            ebx7Feature[23] = "clflushopt";
+            ebx7Feature[24] = "clwb";
+            ebx7Feature[25] = "intel_pt";
+            ebx7Feature[26] = "avx512_pf";
+            ebx7Feature[27] = "avx512_er";
+            ebx7Feature[28] = "avx512_cd";
+            ebx7Feature[29] = "sha";
+            ebx7Feature[30] = "avx512_bw";
+            ebx7Feature[31] = "avx512_vl";
         }
     }
 }
